7d24d7f580caab978dab7bd91fe566c98e64408b
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2025-01-10 09:22:36 -0300

    fix: fixing subtractions of months when turning years (#8)

---
+++ test/unit/bugfixes/bug-7.spec.ts
@@ -0,0 +1,14 @@
+import { SonicDate } from 'src/sonic-date';
+
+describe('incorrect calc', () => {
+	it('2025-01-10T09:10:00Z - 25 years - 1 month should be 25 years ago', () => {
+		const date = new SonicDate('2025-01-10T09:10:00Z');
+
+		const result = date
+			.addYears(-25)
+			.addMonths(-1)
+			.differenceInYears(SonicDate.Now);
+
+		expect(result).toBe(25);
+	});
+});
